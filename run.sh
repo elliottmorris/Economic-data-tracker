fcec000998091e7167f44618a311af4cbcf5335c
Elliott Morris <11248148+elliottmorris@users.noreply.github.com>
2025-04-16 21:31:31 -0400

    updating data

---
+++ run.sh
@@ -0,0 +1,8 @@
+# call model, push output to github
+Rscript _scripts/01_wrangle_fred.R
+Rscript _scripts/02_latent_factor_index.R
+
+# push to github
+git add .
+git commit -m 'updating data'
+git push
