7af1f3451ec12c30e9a50d442401e663713c41ae
DamonCharlesRoberts <dev.properly669@passmail.net>
2025-04-17 21:21:36 -0400

    Adding some inline documentation for the ingestion script.

--- _scripts/01_ingest.py
+++ _scripts/01_ingest.py
@@ -1,7 +1,12 @@
 """Ingest the FRED data.
 
 This script is responsible for pulling in the FRED
-data and setting it up for analysis.
+and SP500 data.
+
+It will only pull in the data for the dates
+that we are currently missing data. It will then
+append our existing data file any new data
+that has come through.
 """
 from datetime import datetime
 from dotenv import load_dotenv
@@ -48,10 +53,9 @@ list_series = [
 host = "https://api.stlouisfed.org/fred/series/observations"
 # - Initialize temp dataframe.
 df_temp = pl.DataFrame()
-# - Make calls.
+# - Make calls for each series.
 logger.success(f"Pulling FRED data between {start_date} - {end_date}")
 for i in list_series:
-    logger.info(f"Accessing data on {i}.")
     # Define the payload.
     params = {
         "series_id":i
@@ -60,13 +64,19 @@ for i in list_series:
         , "observation_start":start_date
         , "observation_end":end_date
     }
+    # If it is not the SP500 data, then make calls to the FRED
+    # API and do the cleaning necessary.
     if i!="^GSPC":
         # Make request.
         resp = get(host, params=params)
+        # If I make a request and the data is empty, move along.
         if len(resp.json()["observations"])==0:
             continue
+        # However, if the response is not empty, clean up the data.
         else:
-            # Add to data.frame
+            # Add response to dataframe and clean.
+            # If the series is the first one, then
+            # I will need to horizontally append the data.
             if i=="AHETPI":
                 df_temp = pl.concat([
                     df_temp
@@ -79,6 +89,8 @@ for i in list_series:
                         .cast({"date":pl.Date})
                     )
                 ], how="horizontal")
+            # However, if it is a subsequent series, then
+            # I will have to do a full join.
             else:
                 df_temp = df_temp.join(
                     (
@@ -93,7 +105,12 @@ for i in list_series:
                     , on="date"
                     , coalesce=True
                 )
+    # If this is the SP500 data, then I will need to do the following.
     elif i=="^GSPC":
+        # If the dataframe from the FRED data is empty, then
+        # I should make the call to the SP500 and store it
+        # in the dataframe. I will overwrite the empty df_temp
+        # DataFrame object.
         if len(df_temp)==0:
             df_temp = (
                 pl.from_pandas(
@@ -110,6 +127,8 @@ for i in list_series:
                 .rename({"Date":"date", "Close":"SP500"})
                 .cast({"date":pl.Date})
             )
+        # However, if there is data from the FRED API, then I will instead
+        # need to make a full join.
         else:
             # Get the SP500 data for GSPC
             df_temp = df_temp.join(
@@ -132,13 +151,19 @@ for i in list_series:
                 , on="date"
                 , coalesce=True
             )
-# If we have data already, just append it.
+# If we have data already, just vertically append it.
+# If I only have SP500 data, then df_temp will be
+# only two columns wide, therefore I will need to
+# fill the other columns as null during the appending.
+# I would do this with diagonal as polars will sort that
+# out and add nulls for me.
 try:
     assert os.path.exists(file_path)
     df = pl.concat([df, df_temp], how="diagonal_relaxed")
+# If we do not have data already, then just rename the
+# df_temp object.
 except AssertionError:
     df = df_temp
-
 # Order by date and remove any duplicate rows.
 df = (
     df
