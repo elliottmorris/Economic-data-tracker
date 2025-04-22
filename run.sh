43419abe8ba360b33c0fa3c029126c7dcab39b83
DamonCharlesRoberts <dev.properly669@passmail.net>
2025-04-21 20:50:16 -0400

    Updating the shell script.

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 # call model, push output to github
-Rscript _scripts/01_wrangle_fred.R
-Rscript _scripts/02_latent_factor_index.R
+uv run python -m ./_scripts/01_ingest.py
+Rscript _scripts/02_wrangle_fred.R
+Rscript _scripts/03_latent_factor_index.R
 
 # push to github
 git add .
-git commit -m 'updating data'
+git commit -m 'updating model'
 git push
